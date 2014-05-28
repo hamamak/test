49455cf1fe6f2cc2914119afda8458be365951cd
HAMA <HAMA@MAK>
2014-05-28 11:05:34 +0200

    Signed-off-by: HAMA <HAMA@MAK>

---
+++ mak/src/main/java/edu/esprit/mak/App.java
@@ -0,0 +1,13 @@
+package edu.esprit.mak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
